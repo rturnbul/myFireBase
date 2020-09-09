e3d66dbc8bff49ada1012d8ee644167a971bd392
DataFire <info@datafire.io>
2020-09-09 11:55:58 -0700

    Edit workspace

---
+++ hello_world.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// Import the DataFire library.
+let datafire = require('datafire');
+
+// Create a new Action.
+module.exports = new datafire.Action({
+  description: "Greets the user.",
+  inputs: [{
+    title: "name",
+    type: "string",
+    default: "world",
+    maxLength: 9
+  }],
+  handler: input => {
+    return "Hellots, " + input.name;
+  },
+});
